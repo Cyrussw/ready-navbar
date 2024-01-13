04c14c3adab89c68a96c5bfd4f8a61cab2c2f3c1
Burak <117117416+Cyrussw@users.noreply.github.com>
2024-01-14 00:06:07 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Sairus | GİRİŞ</title>
+</head>
+
+<body>
+    <header class="header">
+        <a href="/" class="logo">Sairus</a>
+
+        <nav class="navbar">
+            <a href="/">Ana Sayfa</a>
+            <a href="/">Sistem</a>
+            <a href="/">Yönetim</a>
+        </nav>
+    </header>
+</body>
+
+</html>
